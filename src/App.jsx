4ba41d0132b9d3a48881b18079b3b5ba63eac621
arafatGithubCode <md489288@gmail.com>
2023-12-19 10:20:56 +0600

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//pages
+import Auth from "./pages/auth/Auth";
+import ExpenseTracker from "./pages/expense-tracker";
+import Error from "./pages/Error";
+
 function App() {
   return (
     <>
-      <p className="bg-red-500">ok</p>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Auth />} />
+          <Route path="/expense-tracker" exact element={<ExpenseTracker />} />
+          <Route path="*" exact element={<Error />} />
+        </Routes>
+      </Router>
     </>
   );
 }
